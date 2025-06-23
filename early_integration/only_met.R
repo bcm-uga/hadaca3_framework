59c048d252676a589315a85f35a6fb1feaadbaaa
Nicolas HOMBERG <nicolashomberg@gricad-nh.u-ga.fr>
2025-06-23 21:17:00 +0200

    corrected metanalysis

--- early_integration/only_met.R
+++ early_integration/only_met.R
@@ -3,6 +3,7 @@ program_block_EI <- function(rna_unit,met_unit,path_dataset) {
   # prop2 = l_pred$prop2
 
   # prop = Reduce(`+`, list(prop1,prop2)) / length(list(prop1,prop2))
-  met_unit[["ref_scRNA"]] = rna_unit$ref_scRNA
+  # met_unit[["ref_scRNA"]] = rna_unit$ref_scRNA
+  within(met_unit, ref_scRNA <- rna_unit$ref_scRNA)
   return(met_unit)
 }
