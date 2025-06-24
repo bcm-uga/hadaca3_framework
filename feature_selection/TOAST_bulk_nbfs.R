9a0461d712f70578d40cb49f4a76f04e08da8a41
Elise Amblard <elise.amblard@m4x.org>
2025-06-24 22:20:38 +0200

    working Toastbulknbfs FS

--- feature_selection/TOAST_bulk_nbfs.R
+++ feature_selection/TOAST_bulk_nbfs.R
@@ -1,18 +1,27 @@
-
 program_block_FS <- function(data,path_og_dataset='') {
   
   nb_fs_rna = 1e3
-  og_ref_bulkRNA  =  read_all_ref_hdf5(path_og_dataset$ref,to_read = 'ref_bulkRNA')$ref_bulkRNA
+  og_ref_bulkRNA = read_all_ref_hdf5(path_og_dataset$ref, to_read = 'ref_bulkRNA')$ref_bulkRNA
+  
+  if (is.list(data)) {
+    og_ref_bulkRNA = lapply(data, function(x) og_ref_bulkRNA[intersect(rownames(og_ref_bulkRNA),
+                                                                       rownames(x$counts)),])
+    data = mapply(function(x,y) list(counts = x$counts[rownames(y),],
+                                     metadata = x$metadata),
+                  data, og_ref_bulkRNA, SIMPLIFY=F)
+    hvg <- lapply(og_ref_bulkRNA, function(x)
+      TOAST::findRefinx(x, nmarker = min(nrow(x), nb_fs_rna)))
+  } else {
+    og_ref_bulkRNA = og_ref_bulkRNA[intersect(rownames(og_ref_bulkRNA),
+                                              rownames(data)),]
+    data = data[rownames(og_ref_bulkRNA),]
+    hvg <- TOAST::findRefinx(og_ref_bulkRNA, nmarker = min(nrow(og_ref_bulkRNA), nb_fs_rna))
+  }
 
-  hvg <- TOAST::findRefinx(og_ref_bulkRNA, nmarker = min(nrow(og_ref_bulkRNA),nb_fs_rna))
-
-  # multi_data$mix$mix_rna = multi_data$mix$mix_rna[hvg,]
-  # multi_data$ref$ref_bulkRNA = multi_data$ref$ref_bulkRNA[hvg,]
-  # multi_data$ref$ref_scRNA = lapply(multi_data$ref$ref_scRNA, function(x) list(counts = x$counts[hvg,], metadata = x$metadata))
-
-  if(is.list(data)){
-    data = lapply(data, function(x) list(counts = x$counts[hvg,], metadata = x$metadata))
-  }else{
+  if (is.list(data)) {
+    data = mapply(function(x, y) list(counts = x$counts[y,], metadata = x$metadata),
+                  data, hvg, SIMPLIFY=F)
+  } else {
     data = data[hvg,]
   }
   return(data) 
