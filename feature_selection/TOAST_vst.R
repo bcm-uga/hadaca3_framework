62468bb070e40233879d56b9a1341b4b5575e102
Elise Amblard <elise.amblard@m4x.org>
2025-06-25 14:38:28 +0200

    corrected TOAST_vst

--- feature_selection/TOAST_vst.R
+++ feature_selection/TOAST_vst.R
@@ -1,14 +1,12 @@
-
-program_block_FS <- function(data,path_og_dataset) {
+program_block_FS <- function(data, path_og_dataset) {
   
   nb_fs_rna = 1e3
 
-  og_ref_bulkRNA  =  read_all_ref_hdf5(path_og_dataset$ref,to_read = 'ref_bulkRNA')$ref_bulkRNA
-
+  og_ref_bulkRNA = read_all_ref_hdf5(path_og_dataset$ref, to_read = 'ref_bulkRNA')$ref_bulkRNA
 
   determine_variable_genes <- function(mat, n_genes=nb_fs_rna) {
     vst <- log2(mat + median(mat[mat > 0]))
-    var_sorted_genes <- TOAST::findRefinx(vst, nmarker = min(nrow(og_ref_bulkRNA),n_genes))
+    var_sorted_genes <- TOAST::findRefinx(vst, nmarker = min(nrow(og_ref_bulkRNA), n_genes))
     return(var_sorted_genes)
   }
   top_genes <- determine_variable_genes(og_ref_bulkRNA, nb_fs_rna)
@@ -17,9 +15,7 @@ program_block_FS <- function(data,path_og_dataset) {
   # multi_data$mix$mix_rna = multi_data$mix$mix_rna[top_genes,]
   # multi_data$ref$ref_bulkRNA = multi_data$ref$ref_bulkRNA[top_genes,]
 
-
   convert_and_filter_sparse_matrix <- function(x, top_gene_names){
-
     dense_counts <- as.matrix(x$counts)
     common_genes <- intersect(top_gene_names, rownames(dense_counts))
 
@@ -29,10 +25,9 @@ program_block_FS <- function(data,path_og_dataset) {
     return(list(counts = sparse_counts, metadata = x$metadata) )
   }
 
-  if(is.list(data)){
-    # data = lapply(data, function(x) list(counts = x$counts[top_genes,], metadata = x$metadata))
-    data = lapply(data, function(x) convert_and_filter_sparse_matrix(x,top_gene_names))
-  }else{
+  if (is.list(data)) {
+    data = lapply(data, function(x) convert_and_filter_sparse_matrix(x, top_gene_names))
+  } else {
     data = data[top_genes,]
   }
   
