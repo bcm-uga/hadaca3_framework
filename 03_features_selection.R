954c726838d24b3117e5dadd5a4ab74e835734ee
Elise Amblard <elise.amblard@m4x.org>
2025-06-25 11:35:02 +0200

    working FS

--- 03_features_selection.R
+++ 03_features_selection.R
@@ -1,28 +1,20 @@
 source(utils_script)
+source(script_file)
 
-
-
-# omic_name = omic
 omic_name = omic2list_name[[omic]]
 
-
-path_og_dataset= list(mix =path_ogmix,ref = path_ogref )
-
-
+path_og_dataset = list(mix = path_ogmix, ref = path_ogref )
 data = read_hdf5(input_file)[[omic_name]]
 
+block = program_block_FS(data, path_og_dataset)
 
-
-source(script_file)
-block = program_block_FS(data,path_og_dataset)
-
-if(length(block) == 0){
-strop(paste("result data in Features selection is empty, script_file = ",script_file ) )
+if (length(block) == 0) {
+  stop(paste("result data in Feature Selection is empty, script_file = ", script_file))
 } 
 
+res = list()
+res[[omic_name]] = block
 
-res =list()
-res[[omic_name]] =  block
-# write_global(output_file ,ppblock ) 
-write_global_hdf5(output_file ,res ) 
-# write_all_hdf5(output_file,block)
+write_global_hdf5(output_file, res) 
+#write_global(output_file, ppblock) 
+#write_all_hdf5(output_file, ppblock)
