a420eda8e017157b60d372e4beb918be736dfd8e
Elise Amblard <elise.amblard@m4x.org>
2025-06-25 14:42:41 +0200

    working FS

--- feature_selection/TOAST_nbfs.R
+++ feature_selection/TOAST_nbfs.R
@@ -1,18 +1,10 @@
-
-program_block_FS <- function(data,path_og_dataset='') {
+program_block_FS <- function(data, path_og_dataset='') {
   
-  og_ref_met  =  read_all_ref_hdf5(path_og_dataset$ref,to_read = 'ref_met')$ref_met
-  # nb_fs_met=nrow(og_ref$ref_bulkRNA)
-  nb_fs_met=nrow(og_ref_met)
+  og_ref_met = read_all_ref_hdf5(path_og_dataset$ref, to_read = 'ref_met')$ref_met
+  nb_fs_met = nrow(og_ref_met)
 
+  hvp <- TOAST::findRefinx(og_ref_met, nmarker = min(nrow(og_ref_met), nb_fs_met))
 
-  hvp <- TOAST::findRefinx(og_ref_met, nmarker = min(nrow(og_ref_met),nb_fs_met))
-
-  # multi_data$mix$mix_met <- multi_data$mix$mix_met[hvp,]
-  # multi_data$ref$ref_met <- multi_data$ref$ref_met[hvp,]
-
-  
- 
   return(data[hvp,]) 
 }
 
