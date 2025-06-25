7b934c818e00a052db00085fa9e25eb4e399986f
Elise Amblard <elise.amblard@m4x.org>
2025-06-25 09:15:09 +0200

    files dependency works, needs to resolve block dependency

--- preprocessing/sc_scale.R
+++ preprocessing/sc_scale.R
@@ -1,13 +1,12 @@
-program_block_PP <- function(data,path_og_dataset='',omic='') {
-  
-#only for scRNA
-stopifnot(is.list(data))
+program_block_PP <- function(data, path_og_dataset='', omic='') {
+  #only for scRNA
+  stopifnot(is.list(data))
         
-  data<- lapply(data, function(x) {
-      logical_matrix = x$counts > 1000 ; 
-      x$counts[logical_matrix] = 1000;
-      list(counts = x$counts,
-           metadata = x$metadata)})
+  data <- lapply(data, function(x) {
+    logical_matrix = x$counts > 1000
+    x$counts[logical_matrix] = 1000
+    list(counts = x$counts,
+         metadata = x$metadata)})
   
   return(data) 
 }
