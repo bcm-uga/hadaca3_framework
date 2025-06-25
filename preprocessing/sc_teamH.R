38e6bc3069ddb94bf31240984e4ed7766c79ae54
Elise Amblard <elise.amblard@m4x.org>
2025-06-25 17:54:23 +0200

    move sc_teamH to PP instead of FS

---
+++ preprocessing/sc_teamH.R
@@ -0,0 +1,13 @@
+program_block_FS <- function(data, path_og_dataset='') {
+  
+  warning("This method uses a single-cell signature from team H")
+  avg_expression_df <- read.csv("teamH_scSignature.csv", row.names = 1)
+  
+  if (is.list(data)) {
+    data <- list("ref_teamH"=list(counts=SeuratObject::as.sparse(avg_expression_df),
+                                  metadata=data.frame(cell_type=colnames(avg_expression_df),
+                                                      sample=NA)))
+  } else {data = data[intersect(rownames(avg_expression_df), rownames(data)),]}
+ 
+  return(data) 
+}
