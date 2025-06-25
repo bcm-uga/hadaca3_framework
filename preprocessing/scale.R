0156b965f1d3471eea86009566ea83c8a7724051
Elise Amblard <elise.amblard@m4x.org>
2025-06-25 09:14:07 +0200

    files dependency works, needs to resolve block dependency

--- preprocessing/scale.R
+++ preprocessing/scale.R
@@ -1,22 +1,20 @@
-program_block_PP <- function(data,path_og_dataset='',omic='') {
+program_block_PP <- function(data, path_og_dataset='', omic='') {
 
   scale_matrix <- function(mat) {
     mat = sweep(mat, 2, colSums(mat), "/")
     return(mat)
   }
 
-  if(omic == 'ref_scRNA'){
+  if (omic == 'ref_scRNA') {
     data = lapply(data, function(x) {
       list(counts=scale_matrix(x$counts),
-           metadata=x$metadata)
-    })
-  }else{
+           metadata=x$metadata)})
+  } else {
     data = scale_matrix(data)
   }
-  # multi_data$mix$mix_met = scale_matrix(multi_data$mix$mix_met)
   
+  # multi_data$mix$mix_met = scale_matrix(multi_data$mix$mix_met)
   # stopifnot()
-
 
   return(data) 
 }
