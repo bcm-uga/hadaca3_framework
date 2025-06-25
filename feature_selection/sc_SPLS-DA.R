9e89347c72a8128a7b80c29bc3dabe233aa11083
Elise Amblard <elise.amblard@m4x.org>
2025-06-25 09:29:23 +0200

    fixing scPLSDA

--- feature_selection/sc_SPLS-DA.R
+++ feature_selection/sc_SPLS-DA.R
@@ -1,12 +1,13 @@
-
-program_block_FS <- function(data,path_og_dataset='') {
-   
-    if (!(any(c("ref_concat","ref_integrated","ref_cluster","ref_binarypseudobulk_log") %in% names(ref_scRNA)))) {stop("This FS method requires to run the PP with option_sc set to concat, CCAintegration, cluster or binarypseudobulk_log")}
+program_block_FS <- function(data, path_og_dataset='') {
+    
+    og_ref_rna  =  read_all_ref_hdf5(path_og_dataset$ref, to_read = 'ref_met')$ref_met
+    
+    if (!(any(c("ref_concat","ref_integrated","ref_cluster","ref_binarypseudobulk_log") %in% names(data)))) {stop("This FS method requires to run the PP set to concat, CCAintegration, cluster or binarypseudobulk_log")}
     ### SPLS-DA on sc
-    sc_data = ref_scRNA[[1]]
+    sc_data = data[[1]]
     splsda.model <- mixOmics::mint.splsda(t(sc_data$counts), sc_data$metadata$cell_type, 
-                                study = sc_data$metadata$dataset, ncomp = 5,
-                                keepX = rep(400,5))
+                                          study = sc_data$metadata$dataset, ncomp = 5,
+                                          keepX = rep(400,5))
     choose_markers_scRNA <- unique(c(mixOmics::selectVar(splsda.model, comp = 1)$name,
                                      mixOmics::selectVar(splsda.model, comp = 2)$name,
                                      mixOmics::selectVar(splsda.model, comp = 3)$name,
