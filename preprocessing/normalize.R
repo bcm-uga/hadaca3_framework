1e2d0889892f4cffcfabb9df1b41fb115ff35d6e
Elise Amblard <elise.amblard@m4x.org>
2025-06-25 09:25:36 +0200

    files dependency works, needs to resolve block dependency

--- preprocessing/normalize.R
+++ preprocessing/normalize.R
@@ -1,20 +1,20 @@
-program_block_PP <- function(data,path_og_dataset='',omic='') {    
-    # Normalize input matrices
-    #' Normalize a Matrix by Column Sums
-    #'
-    #' @param mat A numeric matrix where rows represent features and columns represent 
-    #'            samples.
-    #'
-    #' @return A numeric matrix with the same dimensions as the input `mat`, where each 
-    #'         column has been normalized so that its elements sum to one.
-    #'
-    normalize_matrix <- function(mat) {
-      mat = sweep(mat, 2, colSums(mat), "/")
-      return(mat)
-    }
-    # multi_data$ref$ref_bulkRNA = normalize_matrix(multi_data$ref$ref_bulkRNA)
-    # multi_data$ref$ref_met = normalize_matrix(multi_data$ref$ref_met)
-    data = normalize_matrix(data)
+program_block_PP <- function(data, path_og_dataset='', omic='') {    
+  #' Normalize a Matrix by Column Sums
+  #'
+  #' @param mat A numeric matrix where rows represent features and columns represent 
+  #'            samples.
+  #'
+  #' @return A numeric matrix with the same dimensions as the input `mat`, where each 
+  #'         column has been normalized so that its elements sum to one.
+  #'
+  normalize_matrix <- function(mat) {
+    mat = sweep(mat, 2, colSums(mat), "/")
+    return(mat)
+  }
+  
+  #multi_data$ref$ref_bulkRNA = normalize_matrix(multi_data$ref$ref_bulkRNA)
+  #multi_data$ref$ref_met = normalize_matrix(multi_data$ref$ref_met)
+  data = normalize_matrix(data)
 
   return(data) 
 }
