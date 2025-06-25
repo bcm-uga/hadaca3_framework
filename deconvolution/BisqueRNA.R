e7bb8b9ad060e7901e5823d3326e6447baec2463
Hugo Barbot <hugo.barbot@agrocampus-ouest.fr>
2025-06-25 17:00:53 +0200

    uncomment working DE and LI (add temporarly only met and rna)

--- deconvolution/BisqueRNA.R
+++ deconvolution/BisqueRNA.R
@@ -2,23 +2,25 @@
 
 program_block_DE = function(uni_data,path_og_dataset='') {
 
-if (!("BisqueRNA" %in% installed.packages())) {
-      install.packages(pkgs = "BisqueRNA", repos = "https://cloud.r-project.org")
-    }
-    if (is.null(metadata)) {stop("BisqueRNA with EI requires to do EI with the spls_sc option")}
-    # generate bulk matrix
-    bulk.eset <- Biobase::ExpressionSet(assayData = as.matrix(ref_all))
-    # generate reference sc matrix
-    sce <- as.matrix(ref_all)
-    phenoData <- data.frame(SubjectName = metadata$sample,
-                 cellType = metadata$cell_type)
-                 
-    rownames(phenoData) = rownames(metadata)
-    sc.eset <- Biobase::ExpressionSet(assayData = sce,
-                            phenoData = Biobase::AnnotatedDataFrame(phenoData))
-    
-    # do deconvolution
-    prop <- BisqueRNA::ReferenceBasedDecomposition(bulk.eset, sc.eset, markers=NULL, use.overlap=F)$bulk.props
+  if (!("BisqueRNA" %in% installed.packages())) {
+    # install.packages(pkgs = "BisqueRNA", repos = "https://cloud.r-project.org")
+    devtools::install_github("cozygene/bisque")
+  }
+  library(BisqueRNA)
+  if (is.null(metadata)) {stop("BisqueRNA with EI requires to do EI with the spls_sc option")}
+  # generate bulk matrix
+  bulk.eset <- Biobase::ExpressionSet(assayData = as.matrix(uni_data$mix))
+  # generate reference sc matrix
+  sce <- as.matrix(uni_data$ref)
+  phenoData <- data.frame(SubjectName = metadata$sample,
+               cellType = metadata$cell_type)
+               
+  rownames(phenoData) = rownames(metadata)
+  sc.eset <- Biobase::ExpressionSet(assayData = sce,
+                          phenoData = Biobase::AnnotatedDataFrame(phenoData))
   
+  # do deconvolution
+  prop <- BisqueRNA::ReferenceBasedDecomposition(bulk.eset, sc.eset, markers=NULL, use.overlap=F)$bulk.props
+
  
 }
--- late_integration/mean_rmse.R
+++ late_integration/mean_rmse.R
@@ -5,21 +5,36 @@ program_block_li <- function(prop1,prop2,path_dataset) {
   # prop1 = l_pred$prop1
   # prop2 = l_pred$prop2
   # path_dataset = l_pred$last_dataset
-  mix = read_all_hdf5(path_dataset,('mix'))$mix$mix_rna
+  mix = read_all_hdf5(path_dataset$mix,('mix'))$mix
+  mix_rna = mix$mix_rna
+  mix_met = mix$mix_met
+  ref = read_all_hdf5(path_dataset$ref,('ref'))$ref
+  ref_bulkRNA = ref$ref_bulkRNA
+  ref_met = ref$ref_met
+  
+  idx_feat_rna = intersect(rownames(mix_rna), rownames(ref_bulkRNA))
+  mix_rna = mix_rna[idx_feat_rna,]
+  ref_bulkRNA = ref_bulkRNA[idx_feat_rna,]
+  idx_feat_met = intersect(rownames(mix_met), rownames(ref_met))
+  mix_met = mix_met[idx_feat_met,]
+  ref_met = ref_met[idx_feat_met,]
+  
 
-  real = mix / colSums(mix)
-  reconstructed = lapply(list(prop1, prop2), function(x)
-    (ref %*% x) / colSums(ref %*% x))
-  rmse = sapply(reconstructed, function(x) 
-    sqrt(mean(((as.matrix(x)) - as.matrix(real))^2)))
+  real_rna = mix_rna / colSums(mix_rna)
+  reconstructed_rna = (ref_bulkRNA %*% prop1) / colSums(ref_bulkRNA %*% prop1)
+  rmse_rna = sqrt(mean(((as.matrix(reconstructed_rna)) - as.matrix(real_rna))^2))
+    
+  real_met = mix_met / colSums(mix_met)
+  reconstructed_met = (ref_met %*% prop2) / colSums(ref_met %*% prop2)
+  rmse_met = sqrt(mean(((as.matrix(reconstructed_met)) - as.matrix(real_met))^2))
   
   # Normalize RMSEs
-  rmse1_norm = rmse[[1]] / Reduce(`+`, rmse)
-  rmse2_norm = rmse[[2]] / Reduce(`+`, rmse)
+  rmse_rna_norm = rmse_rna / (rmse_rna + rmse_met)
+  rmse_met_norm = rmse_met / (rmse_rna + rmse_met)
   
   # RMSE as weights  
-  prop <- as.matrix(rmse1_norm * prop2 + rmse2_norm  * prop1)
-  # multi_pred <- Reduce("+",l_pred) / length(l_pred)
+  prop <- as.matrix( rmse_met_norm * prop1 + rmse_rna_norm  * prop2 )
+  # if prop1 is prop_rna and prop2 is prop_met
 
   return(prop)
 }
