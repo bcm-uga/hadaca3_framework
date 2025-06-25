4bb2695844a0fa6b5a25dd60baaec6cc42164a27
Elise Amblard <elise.amblard@m4x.org>
2025-06-25 14:40:36 +0200

    working FS

--- feature_selection/TOAST_percent.R
+++ feature_selection/TOAST_percent.R
@@ -1,9 +1,8 @@
-
-program_block_FS <- function(data,path_og_dataset='') {
+program_block_FS <- function(data, path_og_dataset='') {
   
   TOAST_percent_met = 0.8
 
-  og_ref_met  =  read_all_ref_hdf5(path_og_dataset$ref,to_read = 'ref_met')$ref_met
+  og_ref_met = read_all_ref_hdf5(path_og_dataset$ref, to_read = 'ref_met')$ref_met
 
   nmarker_percent = round(TOAST_percent_met*nrow(og_ref_met))
   hvp <- TOAST::findRefinx(og_ref_met, nmarker = nmarker_percent)
