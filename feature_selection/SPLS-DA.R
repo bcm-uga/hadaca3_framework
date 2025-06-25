9cf0fa7084b2864656113aed8de3ad3a5e6530cd
Elise Amblard <elise.amblard@m4x.org>
2025-06-25 14:44:26 +0200

    working FS

--- feature_selection/SPLS-DA.R
+++ feature_selection/SPLS-DA.R
@@ -1,6 +1,6 @@
-program_block_FS <- function(data,path_og_dataset='') {
+program_block_FS <- function(data, path_og_dataset='') {
   
-  og_ref_met  =  read_all_ref_hdf5(path_og_dataset$ref,to_read = 'ref_met')$ref_met
+  og_ref_met = read_all_ref_hdf5(path_og_dataset$ref,to_read = 'ref_met')$ref_met
   
   highly_var_idx <- TOAST::findRefinx(og_ref_met, nmarker=1e4)
   og_ref_met = og_ref_met[highly_var_idx,]
