5cefdec990553d9f81e0a1bca880f3b1efe7fddb
Elise Amblard <elise.amblard@m4x.org>
2025-06-25 09:27:05 +0200

    files dependency works, needs to resolve block dependency

--- preprocessing/LogNorm.R
+++ preprocessing/LogNorm.R
@@ -1,10 +1,10 @@
-program_block_PP <- function(data,path_og_dataset='',omic='') {
+program_block_PP <- function(data, path_og_dataset='', omic='') {
   
-    if(omic == 'ref_scRNA' ){ # is.list(data)
+  if (omic == 'ref_scRNA') { # is.list(data)
     data <- lapply(data, function(x) list(counts=Seurat::LogNormalize(x$counts), metadata=x$metadata))
-    }else{
+  } else {
     data = exp(Seurat::LogNormalize(data))
-
-    }
+  }
+  
   return(data) 
 } 
