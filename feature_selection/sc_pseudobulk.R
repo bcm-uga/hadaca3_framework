5dd3160289023142cbbc78bbd5071eee65f07120
EliseAld <40570458+EliseAld@users.noreply.github.com>
2025-07-15 14:42:54 +0200

    typo syntaxe

--- feature_selection/sc_pseudobulk.R
+++ feature_selection/sc_pseudobulk.R
@@ -2,7 +2,7 @@ program_block_FS <- function(data, path_og_dataset='') {
   
   if (is.list(data)) {
     sc = data
-  } else {sc = read_hdf5(path_og_dataset$ref)$ref_scRNA}
+  } else {sc = read_hdf5(path_og_dataset$ref)}
   
   #if (!(any(c("ref_concat","ref_integrated","ref_cluster","ref_binarypseudobulk_log") %in% names(sc)))) {stop("This FS method requires to run the PP set to concat, CCAintegration, cluster or binarypseudobulk_log")}
   
