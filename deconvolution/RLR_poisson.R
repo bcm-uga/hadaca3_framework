70afe7264ef6fdb3e898957839a4c8819cba3986
Nicolas HOMBERG <nicolas.homberg@univ-grenoble-alpes.fr>
2025-08-06 15:33:19 +0200

    try to fix RLR poisson

--- deconvolution/RLR_poisson.R
+++ deconvolution/RLR_poisson.R
@@ -10,21 +10,71 @@ program_block_DE <- function(uni_data,path_og_dataset='') {
     weights = 1/apply(ref, 1, mean)
     return(weights/sum(weights))
   }
-  compute_rlr_weighted <- function(beta.m, ref.m, weights) {
+
+  #Og version 
+  # compute_rlr_weighted <- function(beta.m, ref.m, weights) {
+  #   est.m <- matrix(nrow = ncol(beta.m), ncol = ncol(ref.m))
+  #   colnames(est.m) <- colnames(ref.m)
+  #   rownames(est.m) <- colnames(beta.m)
+  #   for (s in seq_len(ncol(beta.m))) {
+  #     rlm.o <- MASS::rlm(beta.m[, s] ~ ref.m, maxit = 50, weights = weights)
+  #     coef.v <- summary(rlm.o)$coef[2:(ncol(ref.m) + 1), 1]
+  #     weight_rlm <- data.frame(GeneNames = rownames(beta.m), w = rlm.o$w)
+  #     relevant_weights <- weight_rlm$w[weight_rlm$w ==1]
+  #     kept_genes <- weight_rlm$GeneNames[weight_rlm$w ==1]
+  #     # normalisation: remove negative coefficients and enforce the unit-simplex constraint
+  #     coef.v[which(coef.v < 0)] <- 0
+  #     total <- sum(coef.v)
+  #     coef.v <- coef.v/total
+  #     est.m[s, ] <- coef.v
+  #   }
+  #   return(t(est.m))
+  # }
+
+
+# this function remove column if the correlaction is to high for rlm to perform. 
+  compute_rlr_weighted <- function(beta.m, ref.m, weights, correlation_cutoff = 0.99) {
+  # library(MASS)
+  # library(caret)
+
     est.m <- matrix(nrow = ncol(beta.m), ncol = ncol(ref.m))
     colnames(est.m) <- colnames(ref.m)
     rownames(est.m) <- colnames(beta.m)
+
     for (s in seq_len(ncol(beta.m))) {
-      rlm.o <- MASS::rlm(beta.m[, s] ~ ref.m, maxit = 50, weights = weights)
-      coef.v <- summary(rlm.o)$coef[2:(ncol(ref.m) + 1), 1]
-      weight_rlm <- data.frame(GeneNames = rownames(beta.m), w = rlm.o$w)
-      relevant_weights <- weight_rlm$w[weight_rlm$w ==1]
-      kept_genes <- weight_rlm$GeneNames[weight_rlm$w ==1]
-      # normalisation: remove negative coefficients and enforce the unit-simplex constraint
-      coef.v[which(coef.v < 0)] <- 0
-      total <- sum(coef.v)
-      coef.v <- coef.v/total
-      est.m[s, ] <- coef.v
+      current_ref <- ref.m
+      ref_cols <- colnames(ref.m)
+      success <- FALSE
+
+      while (!success) {
+        # Try fitting the robust linear model
+        tryCatch({
+          rlm.o <- MASS::rlm(beta.m[, s] ~ current_ref - 1, maxit = 50, weights = weights)
+          coef.v <- summary(rlm.o)$coef[, 1]
+
+          # Normalize: remove negatives and enforce sum to 1
+          coef.v[coef.v < 0] <- 0
+          total <- sum(coef.v)
+          coef.v <- coef.v / total
+
+          # Store results
+          est.m[s, ] <- NA
+          est.m[s, colnames(current_ref)] <- coef.v
+          success <- TRUE
+        }, error = function(e) {
+          message(paste("RLM failed for sample", s, "- removing correlated columns..."))
+
+          # Remove highly correlated columns
+          cor_mat <- cor(current_ref)
+          to_remove <- findCorrelation(cor_mat, cutoff = correlation_cutoff)
+
+          if (length(to_remove) == 0) {
+            stop("RLM failed and no correlated columns left to remove.")
+          }
+
+          current_ref <- current_ref[, -to_remove, drop = FALSE]
+        })
+      }
     }
     return(t(est.m))
   }
